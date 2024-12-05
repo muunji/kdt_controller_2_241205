7e3c3fea4639a37d75a74f2dda7ed5f97172835e
muunji <twoji3019@naver.com>
2024-12-05 18:02:48 +0900

    Revert "객체로 만들기 + json출력"
    This reverts commit 8d8b3c96f89507ad37e9c6f653ad2de4e3a5be73.

--- src/server.js
+++ src/server.js
@@ -23,36 +23,24 @@ const server = http.createServer((req,res)=> {
     if(req.url === "/text") {
       req.on("data",(chunk)=>{
         //한글로 받아오기
-        const string = decodeURI(chunk.toString());
+        const string = decodeURI(chunk.toString('utf-8'));
         const key =string.split('=');
-        //객체로 넣기
-        body[key[0]] = key[1];
+        console.log(key[0]);
+        // console.log(JSON.stringify(key));
+        body+=decodeURI(chunk.toString('utf-8'));
       });
       req.on("end",()=>{
         console.log(body);
 
-        let existData = [];
-
-        //파일이 존재한다면
-        if(fs.existsSync("text.json")){
-          const fileContent = fs.readFileSync("text.json","utf-8");
-          existData += JSON.parse(fileContent);
-          return existData;
+        if(body!==""){
+          fs.writeFile("text.txt",body,'utf-8',(err)=>{
+            if(err){
+              console.error(err);
+              return;
+            }
+            console.log("make file");
+          })
         }
-
-        //기존 파일에 데이터 추가
-        existData.push(body);
-
-        //파일에 json 데이터 저장
-        fs.writeFileSync("text.json",JSON.stringify(existData),"utf-8",(err)=>{
-          if(err){
-            console.error(err);
-            return
-          }
-          console.log("make JSON file");
-        });
-        res.writeHead(200,{"content-type":"application/json"});
-        res.end();
 
       const pageData = fs.readFileSync(path.join(__dirname,"/public/index.html"),'utf-8',()=>{});
       res.writeHead(200,{"content-type":"text/html"});
