fda05619c7a00a7bb7dc215e51ba3d3468e9ff2a
muunji <twoji3019@naver.com>
2024-12-09 10:10:14 +0900

    파일생성

--- src/server.js
+++ src/server.js
@@ -34,28 +34,33 @@ const server = http.createServer((req,res)=> {
           const [key, value] = pair.split('=').map(decodeURIComponent);
           acc[key]=value;
           return acc;
-        },{});
-        console.log("parseData : "+parseData);
+      },{});
 
-        //파일 처리
-        const fileCheck = fs.existsSync(filePath);
-        console.log("fileCheck : "+ fileCheck); //true, false
+      console.log("parseData : "+parseData);
 
-        let jsonData;
+      //파일 처리
+      const fileCheck = fs.existsSync(filePath);
+      cnsole.log("fileCheck : "+ fileCheck); //true, false
 
-        //파일이 존재하면 내용을 읽고 JSON 객체로 변환
-        if(fileCheck) {
-          const fileContent = fs.readFileSync(filePath,'utf-8');
-          jsonData = JSON.parse(fileContent);
-        }
-        //파일이 없다면 빈 객체로 초기화
-        else {
-          jsonData = {};
-        }
+      let jsonData;
+
+      //파일이 존재하면 내용을 읽고 JSON 객체로 변환
+      if(fileCheck) {
+        const fileContent = fs.readFileSync(filePath,'utf-8');
+        jsonData = JSON.parse(fileContent);
+      }
+      //파일이 없다면 빈 객체로 초기화
+      else {
+        jsonData = {};
+      }
         
-        //기본 데이터와 새로운 데이터 합치기
-        jsonData = {...jsonData,...parseData};
+      //기본 데이터와 새로운 데이터 합치기
+      jsonData = {...jsonData,...parseData};
 
+      fs.writeFileSync(filePath,JSON.stringify(jsonData,null,2),'utf-8');
+      console.log("save file");
+
+      //응답
       const pageData = fs.readFileSync(path.join(__dirname,"/public/index.html"),'utf-8',()=>{});
       res.writeHead(200,{"content-type":"text/html"});
       res.write(pageData);
