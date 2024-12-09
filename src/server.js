14c260dda008c1b5592d4c72e19048c63d621c20
muunji <twoji3019@naver.com>
2024-12-09 12:45:01 +0900

    push사용

--- src/server.js
+++ src/server.js
@@ -20,11 +20,11 @@ const server = http.createServer((req,res)=> {
   //POST 메소드
   if(req.method === "POST") {
     //data가 들어오는 곳, json -> 객체
-    let body = "";
-    
     if(req.url === "/text") {
+      let body = "";
+
+      //데이터 누적
       req.on("data",(chunk)=>{
-        //데이터 누적
         body += chunk.toString();
         console.log("body:"+body);
       });
@@ -40,28 +40,21 @@ const server = http.createServer((req,res)=> {
         console.log("fileCheck : "+ fileCheck); //true, false
 
         //파일이 존재하면 내용을 읽고 JSON 객체로 변환
-        let jsonData;
-
+        let jsonData=[];
         if(fileCheck) {
           const fileContent = fs.readFileSync(filePath,'utf-8');
           jsonData = JSON.parse(fileContent);
         }
-        //파일이 없다면 빈 객체로 초기화
-        else {
-          jsonData = {};
-        }
-        
-        //기본 데이터와 새로운 데이터 합치기
-        jsonData = {...jsonData,...parseData};
+
+        //parseData를 배열로 추가
+        jsonData.push(parseData);
 
         fs.writeFileSync(filePath,JSON.stringify(jsonData,null,2),'utf-8');
         console.log("save file");
 
         //응답
-        const pageData = fs.readFileSync(path.join(__dirname,"/public/index.html"),'utf-8',()=>{});
-        res.writeHead(200,{"content-type":"text/html"});
-        res.write(pageData);
-        res.end();
+        res.writeHead(200,{"content-type":"application/json"});
+        res.end(JSON.stringify({message: "save success"}));
       })
     }
   }
