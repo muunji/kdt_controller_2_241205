c34eb046a97d533df0ef86b30dd2de7a2b0c6621
muunji <twoji3019@naver.com>
2024-12-09 10:57:10 +0900

    querystring 사용

--- src/server.js
+++ src/server.js
@@ -21,50 +21,47 @@ const server = http.createServer((req,res)=> {
   if(req.method === "POST") {
     //data가 들어오는 곳, json -> 객체
     let body = "";
-    //form data
+    
     if(req.url === "/text") {
       req.on("data",(chunk)=>{
-       //데이터 누적
-       body += chunk.toString();
-      });
-      req.on("end",()=>{
+        //데이터 누적
+        body += chunk.toString();
         console.log("body:"+body);
+      });
+
+      req.on("end",()=>{
+
         //데이터 파싱
-        const parseData = body.split('&').reduce((acc,pair)=>{
-          const [key, value] = pair.split('=').map(decodeURIComponent);
-          acc[key]=value;
-          return acc;
-      },{});
+        const parseData= querystring.parse(body);
+        console.log("parseData: "+pageData);
 
-      console.log("parseData : "+parseData);
+        //파일 처리
+        const fileCheck = fs.existsSync(filePath);
+        console.log("fileCheck : "+ fileCheck); //true, false
 
-      //파일 처리
-      const fileCheck = fs.existsSync(filePath);
-      console.log("fileCheck : "+ fileCheck); //true, false
+        //파일이 존재하면 내용을 읽고 JSON 객체로 변환
+        let jsonData;
 
-      let jsonData;
-
-      //파일이 존재하면 내용을 읽고 JSON 객체로 변환
-      if(fileCheck) {
-        const fileContent = fs.readFileSync(filePath,'utf-8');
-        jsonData = JSON.parse(fileContent);
-      }
-      //파일이 없다면 빈 객체로 초기화
-      else {
-        jsonData = {};
-      }
+        if(fileCheck) {
+          const fileContent = fs.readFileSync(filePath,'utf-8');
+          jsonData = JSON.parse(fileContent);
+        }
+        //파일이 없다면 빈 객체로 초기화
+        else {
+          jsonData = {};
+        }
         
-      //기본 데이터와 새로운 데이터 합치기
-      jsonData = {...jsonData,...parseData};
+        //기본 데이터와 새로운 데이터 합치기
+        jsonData = {...jsonData,...parseArrData};
 
-      fs.writeFileSync(filePath,JSON.stringify(jsonData,null,2),'utf-8');
-      console.log("save file");
+        fs.writeFileSync(filePath,JSON.stringify(jsonData,null,2),'utf-8');
+        console.log("save file");
 
-      //응답
-      const pageData = fs.readFileSync(path.join(__dirname,"/public/index.html"),'utf-8',()=>{});
-      res.writeHead(200,{"content-type":"text/html"});
-      res.write(pageData);
-      res.end();
+        //응답
+        const pageData = fs.readFileSync(path.join(__dirname,"/public/index.html"),'utf-8',()=>{});
+        res.writeHead(200,{"content-type":"text/html"});
+        res.write(pageData);
+        res.end();
       })
     }
   }
